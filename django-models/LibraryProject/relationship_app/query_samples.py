55f1c185d112dc547057288a8351bde27a1860da
Abisoye <theabisoyelawal@gmail.com>
2026-01-16 01:52:07 +0100

    Checker-approved queries for all tasks in relationship_app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,4 +13,5 @@ def get_books_in_library(library_name):
 # 3️⃣ Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian.name
+    librarian = Librarian.objects.get(library=library)
+    return librarian.name
