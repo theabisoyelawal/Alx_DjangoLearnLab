6f67fd2ec9305222ad85a2f5e08b2cd64328604f
Abisoye <theabisoyelawal@gmail.com>
2026-01-16 01:44:43 +0100

    Fix queries in query_samples.py to pass ALX checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,29 +1,14 @@
-# LibraryProject/relationship_app/query_samples.py
-
-from LibraryProject.relationship_app.models import Author, Book, Library, Librarian
+from .models import Author, Book, Library, Librarian
 
 # 1️⃣ Query all books by a specific author
 def get_books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        return author.books.all()
-    except Author.DoesNotExist:
-        return f"No author found with the name '{author_name}'"
+    return Book.objects.filter(author__name=author_name)
 
 # 2️⃣ List all books in a library
 def get_books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.books.all()
-    except Library.DoesNotExist:
-        return f"No library found with the name '{library_name}'"
+    return Book.objects.filter(library__name=library_name)
 
 # 3️⃣ Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.librarian
-    except Library.DoesNotExist:
-        return f"No library found with the name '{library_name}'"
-    except Librarian.DoesNotExist:
-        return f"No librarian assigned to '{library_name}'"
+    librarian = Librarian.objects.get(library__name=library_name)
+    return librarian.name
