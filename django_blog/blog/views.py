23cdf9fb460732d722457d41437709f714ec7f9e
Abisoye <theabisoyelawal@gmail.com>
2026-02-10 23:33:07 +0100

    Fix CRUD URLs, templates, and permission mixins

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,10 +1,4 @@
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView
-)
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Post
@@ -13,7 +7,6 @@ from .models import Post
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
 
 
 class PostDetailView(DetailView):
