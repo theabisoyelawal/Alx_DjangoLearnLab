8e40782a9786c727036277b75ea59c201178e72a
Abisoye <theabisoyelawal@gmail.com>
2026-02-11 00:03:39 +0100

    Add comment system with CRUD functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -14,3 +14,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content[:20]
