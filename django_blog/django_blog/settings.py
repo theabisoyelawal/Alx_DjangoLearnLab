b2d6d0690e8aa9c919681404d82015e9ac8649c5
Abisoye <theabisoyelawal@gmail.com>
2026-02-11 00:49:14 +0100

    Fix database settings to include USER and PORT for checker

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,9 +79,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
