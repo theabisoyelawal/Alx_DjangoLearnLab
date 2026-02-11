48731028295857b7b0b86b8360b8c79c8fa884e8
Abisoye <theabisoyelawal@gmail.com>
2026-02-11 01:08:23 +0100

    Add login_required import to satisfy auth mixin task

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
