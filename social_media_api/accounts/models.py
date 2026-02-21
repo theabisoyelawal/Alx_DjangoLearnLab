b5b2879c22cd2fb3c3d571ee022eb79dd47ef103
Abisoye <theabisoyelawal@gmail.com>
2026-02-21 19:48:00 +0100

    Task 1 + Task 2: fixed custom user, registration, login, profile, follow/unfollow

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
-    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
     def __str__(self):
         return self.username
--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,37 +4,38 @@ from rest_framework.authtoken.models import Token
 
 CustomUser = get_user_model()
 
+# ---------------------------
+# Registration
+# ---------------------------
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email'),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
+            profile_picture=validated_data.get('profile_picture')
         )
+        Token.objects.create(user=user)
         return user
 
+# ---------------------------
+# Login
+# ---------------------------
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
 
+# ---------------------------
+# Profile
+# ---------------------------
 class ProfileSerializer(serializers.ModelSerializer):
-    followers_count = serializers.SerializerMethodField()
-    following_count = serializers.SerializerMethodField()
-
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers_count', 'following_count']
-
-    def get_followers_count(self, obj):
-        return obj.followers.count()
-
-    def get_following_count(self, obj):
-        return obj.following.count()
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
